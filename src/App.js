0b1d658da66be9fa9fbdbe35a702a17b7a9ee6fb
Marik <marik@Mariks-MacBook-Pro.local>
2022-01-18 17:31:16 -0500

    removed useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import styled from "styled-components";
 import MovieComponent from "./components/MovieComponent.js";
 import MovieInfoComponent from './components/MovieInfoComponent.js'
